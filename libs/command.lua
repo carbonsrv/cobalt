294ac4188008505b46b58bffdc2375183a9e435f
Adrian Pistol <adinator1999@gmail.com>
2015-12-18 22:19:53 +0100

    Small cleanup.

---
+++ libs/command.lua
@@ -0,0 +1,51 @@
+-- Command handler and stuff.
+
+local _M = {}
+function _M.start_handler()
+	event.handle("irc:privmsg", function(server, from, to, message)
+		local handler = function(cmd, match)
+			if cmd and cmd ~= "" then
+				local sender = from:match("^(.-)!")
+				local sendto = sender == irc_set[server].nick and sender or to
+
+				-- Arguments
+				local replyargs = {"irc.msg", server, sendto}
+
+				event.fire("chatcmd:"..cmd, replyargs, from, match)
+
+				return ""
+			end
+		end
+		local matched = message:gsub("^"..irc_set[server].prefix.."(.-) (.*)$", handler) ~= message
+		if not matched then
+			message:gsub("^"..irc_set[server].prefix.."(.+)", handler)
+		end
+	end, {
+		irc_set = settings.irc,
+	})
+end
+
+function _M.add(name, func, binds)
+	local args =  {
+		fn = string.dump(func)
+	}
+	if binds then
+		for k, v in pairs(binds) do
+			if k ~= "fn" then
+				args[k] = v
+			end
+		end
+	end
+	event.handle("chatcmd:"..name, function(replyargs, ...)
+		cmd_func = cmd_func or loadstring(fn)
+		local suc, res = pcall(cmd_func, ...)
+		if suc then
+			table.insert(replyargs, res)
+			rpc.call(unpack(replyargs))
+		else
+			print("Error: "..res)
+		end
+	end, args)
+end
+
+return _M
