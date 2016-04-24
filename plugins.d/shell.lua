2576d88a4eb7c3c1e426073ef71790cdd714d1ef
Adrian Pistol <vifino@tty.sh>
2016-04-24 02:16:36 +0200

    Small shell command improvement.

--- plugins.d/shell.lua
+++ plugins.d/shell.lua
@@ -4,7 +4,7 @@ command.add("sh", function(from, chan, args)
 	if perms[from] == 3 then
 		-- Authorized.
 			local output = io.popen(args.." 2>&1"):read("*a")
-			return "| "..output:gsub("\n$", ""):gsub("\n", "\n| ")
+			return "| "..output:gsub("(\n+)$", ""):gsub("(\n+)", "\n| "):gsub("\t", "    ")
 	else
 		return "How about no?"
 	end
